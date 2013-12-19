704d3d6ce778c36a1a7a1b03320635bbe36abd10
mezbahalam <mezbahalam26@gmail.com>
2013-12-19 21:56:58 +0600

    add4

--- config/environment.rb
+++ config/environment.rb
@@ -1,16 +1,5 @@
-configure :development do
-  set :database, "sqlite3:///database.db"
-end
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
 
-configure :production do
-  db = URI.parse(ENV['HEROKU_POSTGRESQL_BLUE_URL '] || 'postgres://localhost/mydb')
-
-  ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-  )
-end
+# Initialize the Rails application.
+Apple::Application.initialize!
